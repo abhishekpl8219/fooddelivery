7e3f98e76fac0f202edbae57a12f9aa23aca00f2
abhishek <abhishekpl8219@gmail.com>
2025-09-04 21:40:56 +0530

    added tabs navigation.

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import useAuthStore from "@/store/auth.store";
 import { Redirect } from "expo-router";
 
 export default function Index() {
-//   const isAuthenticated = false; // replace with real state later
+  //   const isAuthenticated = false; // replace with real state later
   const { isAuthenticated } = useAuthStore();
 
   return <Redirect href={isAuthenticated ? "/(tabs)" : "/(auth)/sign-in"} />;
