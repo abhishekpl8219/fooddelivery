d6afe23451cdd3cab2b90bda256d021cee7a5523
abhishek <abhishekpl8219@gmail.com>
2025-09-06 17:50:49 +0530

    added cart store feature.

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,15 +1,17 @@
 import { images } from "@/constants";
+import { useCartStore } from "@/store/cart.store";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const CartButton = () => {
-  const totalitems = 10;
+  const { getTotalItems } = useCartStore();
+  const totalItems = getTotalItems();
   return (
     <TouchableOpacity className="cart-btn">
       <Image source={images.bag} className="size-5" resizeMode="contain" />
-      {totalitems > 0 && (
+      {totalItems > 0 && (
         <View className="cart-badge">
-          <Text className="text-white small-bold">{totalitems}</Text>
+          <Text className="text-white small-bold">{totalItems}</Text>
         </View>
       )}
     </TouchableOpacity>
